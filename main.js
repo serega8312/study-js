5f679405ab230ee7738f5f44a7da17e31e2d1a18
Serega <seregamsv929@gmail.com>
2022-04-19 19:29:51 +0300

    Создали post

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ $(document).ready(function () {
   $('#modaldemo').click(function () {
     $('#content').load("modaldemo/modal.html");
   })
-  $.getJSON("http://jsonplaceholder.typicode.com/posts", function (json) {
-    for (let i in json) {
-      let post = json[i];
-      $('#content').append(`<div class="card"> <div class="card-body">${post.body}</div></div>`);
-    }
-  });
+  $('#posts').click(function () {
+    $.getJSON("http://jsonplaceholder.typicode.com/posts", function (json) {
+      $('#content').html("");
+      for (let i in json) {
+        let post = json[i];
+        $('#content').append(`<div class="card"> <div class="card-body">${post.body}</div></div>`);
+
+      }
+    });
+  })
+
 });
