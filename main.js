aaeeaad031236379aa7724aa303b8a7b34afd452
Serega <seregamsv929@gmail.com>
2022-04-19 10:38:16 +0300

    загрузили джейсон

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
-  $('#modaldemo').click(function (){
+  $('#modaldemo').click(function () {
     $('#content').load("modaldemo/modal.html");
   })
-
+  $.getJSON("http://jsonplaceholder.typicode.com/posts", function (json) {
+    for (let i in json) {
+      let post = json[i];
+      $('#content').append(`<div class="card"> <div class="card-body">${post.body}</div></div>`);
+    }
+  });
 });
