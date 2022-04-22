749127f31808b5d95432ad12d4f5e839109475d0
Serega <seregamsv929@gmail.com>
2022-04-22 20:19:02 +0300

    ДОбавили changeDisplay.js

---
+++ changeDisplay.js
@@ -0,0 +1,28 @@
+function removeDisplay() {
+  say('снять дислей');
+}
+function cleanGlue() {
+  say('отчистиь клей');
+}
+function disassemblePhone() {
+  say('разобрать телефон');
+}
+function stickDisplay() {
+  say('наклеить дисплей');
+}
+function assemblePhone() {
+  say('собрать телефон');
+}
+function turnOnPhone() {
+  say('включить телфон');
+}
+
+function changeDisplay() {
+  removeDisplay();
+  cleanGlue();
+  disassemblePhone();
+  stickDisplay();
+  assemblePhone();
+  turnOnPhone();
+
+}
--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+  $('#changeDisplay').click(function () {
+    $('#content').html("");
+    changeDisplay();
+  })
   $('#dinner').click(function () {
     $('#content').html("");
     dinner();
@@ -19,3 +23,7 @@ $(document).ready(function () {
 
 });
 
+function say(text, cssClass) {
+  $('#content').append(`<div class="card ${cssClass}"> <div class="card-body">${text}</div></div>`);
+
+}
