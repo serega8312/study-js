564d39c358a80470127aca83a52ce400338b7157
Serega <seregamsv929@gmail.com>
2022-04-27 10:32:56 +0300

    Изучали константу

--- main.js
+++ main.js
@@ -5,10 +5,39 @@
 // рейди это функция  в которую передаетя безимянная функция
 $(document).ready(function () {
 
-  // оброботчик события клик  на html элементс айди revers (пункт-меню) '#revers'
+  // оброботчик события клик  на html элемент с айди revers (пункт-меню) '#revers'
   //'#revers' пример css селектора по айди
+  // клик это функция в которую передается безымянная функция
+  //
   $('#revers').click(function () {
-    const bla = $('#talktext').val();
+
+    //загнать в константу чило 5
+    const a = 5;
+    const b = "test";
+    const c = 7;
+    const d = (a + c) * a;
+    const e = {};
+    const r = {
+      name: 'bob'
+    };
+    const s = {
+      age: 35,
+      name: "bob"
+    };
+    const f = [];
+    const g = [r, s];
+    const h = function () { };
+    const j = s.age;
+    const k = {
+      firstName: r.name, lastName: "art",
+      // fullName:function(){
+      //   return
+      // }
+
+    }
+    //загнать  в константу  html элемент
+    const talktext = $('#talktext');
+    const bla = talktext.val();
     const rev = reverseString(bla);
     $("#talkh1").text(rev);
 
