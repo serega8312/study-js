32c3cd062c18d97f046b82ce75e2eea484ec508a
Serega <seregamsv929@gmail.com>
2022-05-06 10:55:21 +0300

    Cоздали функцию лайн

--- task1.js
+++ task1.js
@@ -1,21 +1,33 @@
-const words = ['time', 'love', 'there'];
+const words = ['time', 'love', 'there', 'chance'];
 
 
 $(document).ready(function () {
-
-
 
   $('#check').click(function () {
 
     for (let i in words) {
       check('#input' + i, words[i]);
     }
+    
   })
 
 
 
 })
 
+function line(id,label){
+  $('#content').prepend(`
+  <div class="form-group row mb-1">
+    <label for="staticEmail" class="col-sm-1 col-form-label">${label}</label>
+    <div class="col-sm-6">
+      <input type="text" class="form-control" id="${id}">
+    </div>
+  </div>`);
+}
+line('input0','Время');
+line('input1','Любовь');
+line('input2','Там');
+line('input3','Шанс');
 
 function check(inputId, rightValue) {
   const input = $(inputId);
@@ -26,3 +38,4 @@ function check(inputId, rightValue) {
     input.addClass('bg-danger');
   }
 }
+
