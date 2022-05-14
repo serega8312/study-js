38e457e422627bca34b77cbb241e0e5cbaf07b70
Serega <seregamsv929@gmail.com>
2022-05-14 21:07:48 +0300

    добавили таск для скорпионс

--- song.js
+++ song.js
@@ -48,6 +48,13 @@ const songId = searchParams.get("v")
 
 $.getJSON(`data/${songId}.json`, function (json) {
 
+  for (let i in json.tasks) {
+    const task = json.tasks[i]
+    $('#tasks').append(`<li><a class="dropdown-item" href="${task.href}">${task.text}</a></li>`);
+
+
+  }
+
   if (json.background.image) $("body").attr("style", "background-image:" + json.background.image);
   if (json.background.color) $("body").attr("style", "background-color:" + json.background.color);
 
